592abf8d036168df01daa70fad9e3a717174264a
General-Monish <monissam28@gmail.com>
2025-08-18 03:26:42 +0530

    Added UI and GM Script

--- JumperGame/GameManager.cs
+++ JumperGame/GameManager.cs
@@ -1,10 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField] GameObject spawnPoint;
+    [SerializeField] Transform spawnPoint;
+    [SerializeField] GameObject obstacles;
+
+    int score = 0;
+
+    [SerializeField] GameObject playButton;
+    [SerializeField] TextMeshProUGUI scoreText;
 
     // Start is called before the first frame update
     void Start()
@@ -16,5 +23,31 @@ public class GameManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void GameStart()
+    {
+        // Hide the play button
+        playButton.SetActive(false);
+        StartCoroutine("SpawnObstacles");
+        InvokeRepeating("UpdateScore", 1f, 1f); // Update score every second  
+
+    }
+
+    IEnumerator SpawnObstacles()
+    {
+        while (true)
+        {
+            // Check if the game is running
+            float waitTime = Random.Range(0.5f, 1.5f);
+            yield return new WaitForSeconds(waitTime);
+            Instantiate(obstacles, spawnPoint.position, Quaternion.identity);
+        }
+    }
+
+    void UpdateScore()
+    {
+        score++;
+        scoreText.text = score.ToString();
     }
 }
